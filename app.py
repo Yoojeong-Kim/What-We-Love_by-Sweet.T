542eb2897d916742487c60422457b44e91a8eaa6
Yoojeong Kim <yoojung0425@g.skku.edu>
2025-09-18 14:20:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ import streamlit as st
 import openai
 import os
 
+# add style
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+local_css(".streamlit/style.css")
+
 # Set a title for the Streamlit app
 st.title("Nevertheless Chatbot")
 st.markdown("---")
